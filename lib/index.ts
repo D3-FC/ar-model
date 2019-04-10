c0af830f828a6c3e54ccaaa6b7407698c679b0f8
D3-FC <D3-FC@mail.ru>
2019-04-10 18:55:24 +0400

    feat: expose collection from package

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,5 @@
 export { ApiContract } from '../src/modules/Api/ApiContract'
 export { LaravelApi } from '../src/modules/Api/LaravelApi'
 export { Dto } from '../src/modules/DAO/Dto'
+export { ArCollection } from '../src/modules/Collection/ArCollection'
 export { default as ArModel } from '../src/modules/Model/ArModel'
