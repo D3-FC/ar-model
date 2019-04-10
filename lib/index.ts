24c8d35c08a6fb38f7dc844be9bdb0f432380cfc
D3-FC <D3-FC@mail.ru>
2019-04-10 17:11:01 +0400

    feat: expose DTO from lib

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export { ApiContract } from '../src/modules/Api/ApiContract'
 export { LaravelApi } from '../src/modules/Api/LaravelApi'
+export { Dto } from '../src/modules/DAO/Dto'
 export { default as ArModel } from '../src/modules/Model/ArModel'
